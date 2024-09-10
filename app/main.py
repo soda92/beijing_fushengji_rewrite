0023f6e6b4da6ce2a45c2f1ba20e09330ac8fedb
Maple <wang-jiaxun@outlook.com>
2024-09-10 15:30:54 +0800

    add pyinstaller entry

--- app/main.py
+++ app/main.py
@@ -7,20 +7,25 @@ from ui.main import Ui_MainWindow
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.showTipsAtStartup()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.hide()
+        self.show_intro()
 
-    def showTipsAtStartup(self):
+    def show_intro(self):
         self.dlg = StoryDlg()
         self.dlg.start_sig.connect(self.check_start)
         self.dlg.show()
 
     def check_start(self):
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
         self.show()
 
 
-if __name__ == "__main__":
+def main():
     app = QtWidgets.QApplication(sys.argv)
     window = MainWindow()
     sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
