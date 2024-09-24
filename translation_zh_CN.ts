9a8b23520628317fedff57fc93364c2bd245f1df
Jua-Shun Wong <wjxa20152015@gmail.com>
2024-09-24 09:28:18 +0800

    add netcafe text and translation

--- translation_zh_CN.ts
+++ translation_zh_CN.ts
@@ -4,9 +4,44 @@
 <context>
     <name>Dialog</name>
     <message>
-        <location filename="form/net_cafe.ui" line="17"/>
         <source>net_bar_title</source>
-        <translation>街頭網吧</translation>
+        <translation type="vanished">街頭網吧</translation>
+    </message>
+    <message>
+        <location filename="form/net_cafe.ui" line="17"/>
+        <source>Cyber Cafe</source>
+        <translation>街头网吧</translation>
+    </message>
+    <message>
+        <location filename="form/net_cafe.ui" line="23"/>
+        <source>Please choose what to do in cybercafe:</source>
+        <translation>请选择在网吧做什么：</translation>
+    </message>
+    <message>
+        <location filename="form/net_cafe.ui" line="33"/>
+        <source>About this game:</source>
+        <translation>本游戏相关</translation>
+    </message>
+    <message>
+        <location filename="form/net_cafe.ui" line="39"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Write email to author: &lt;a href=&quot;mailto:guoxh@hotmail.com&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;mailto:guoxh@hotmail.com&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;p&gt;&lt;a href=&quot;mailto:guoxh@hotmail.com&quot;&gt;&lt;/a&gt;&lt;span&gt;Join discuss group, get latest version and news:&lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;a href=&quot;http://groups.yahoo.com/group/BeijingHell&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;http://groups.yahoo.com/group/BeijingHell&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;p&gt;Access main page of this game:&lt;/p&gt;&lt;p&gt;&lt;a href=&quot;http://www.guoly.com&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;http://www.guoly.com&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;p&gt;&lt;a href=&quot;http://guolycomtypeb.51.net&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;http://guolycomtypeb.51.net&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <oldsource>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Write email to author: &lt;a href=&quot;mailto:guoxh@hotmail.com&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;mailto:guoxh@hotmail.com&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;p&gt;&lt;a href=&quot;mailto:guoxh@hotmail.com&quot;&gt;&lt;span style=&quot; color:#000000;&quot;&gt;Join discuss group, get latest version and news:&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;p&gt;&lt;a href=&quot;http://groups.yahoo.com/group/BeijingHell&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;http://groups.yahoo.com/group/BeijingHell&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;p&gt;Access main page of this game:&lt;/p&gt;&lt;p&gt;&lt;a href=&quot;http://www.guoly.com&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;http://www.guoly.com&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;p&gt;&lt;a href=&quot;http://guolycomtypeb.51.net&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;http://guolycomtypeb.51.net&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</oldsource>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;给游戏作者写信： &lt;a href=&quot;mailto:guoxh@hotmail.com&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;mailto:guoxh@hotmail.com&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;p&gt;&lt;a href=&quot;mailto:guoxh@hotmail.com&quot;&gt;&lt;/a&gt;&lt;span style=&quot; color:#000000;&quot;&gt;参加讨论组，得到最新版本和最新信息：&lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;a href=&quot;http://groups.yahoo.com/group/BeijingHell&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;http://groups.yahoo.com/group/BeijingHell&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;p&gt;访问|北京浮生记|主页&lt;/p&gt;&lt;p&gt;&lt;a href=&quot;http://www.guoly.com&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;http://www.guoly.com&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;p&gt;&lt;a href=&quot;http://guolycomtypeb.51.net&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;http://guolycomtypeb.51.net&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="form/net_cafe.ui" line="49"/>
+        <source>About game:</source>
+        <translation>游戏相关：</translation>
+    </message>
+    <message>
+        <location filename="form/net_cafe.ui" line="55"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Free game: &lt;a href=&quot;http://www.freegames.com&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;http://www.freegames.com&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;p&gt;Game development: &lt;a href=&quot;http://www.gamedev.net&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;http://www.gamedev.net&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;免费游戏：&lt;a href=&quot;http://www.freegames.com&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;http://www.freegames.com&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;p&gt;游戏开发：&lt;a href=&quot;http://www.gamedev.net&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;http://www.gamedev.net&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="form/net_cafe.ui" line="81"/>
+        <source>Leave Cybercafe</source>
+        <translation>离开网吧</translation>
     </message>
 </context>
 </TS>
