11006d7c17fbed0a59fc4fd6f3eb0fe0d291b2da
Jua-Shun Wong <wjxa20152015@gmail.com>
2024-09-25 16:10:46 +0800

    add translate setting and dialog launch task

--- tools/translate.py
+++ tools/translate.py
@@ -11,7 +11,20 @@ elif pathlib.Path("venv/Scripts/python.exe").exists():
 def run(command):
     subprocess.run(f"{PREFIX}/{command}".split(), check=True)
 
+files1 = pathlib.Path("form").glob("*.ui")
+files1 = [f"form/{x.name}" for x in files1]
 
-run("pyside6-lupdate form/net_cafe.ui console/mainwindow.py -ts translation_zh_CN.ts")
+files2 = pathlib.Path("widgets").glob("*.py")
+files2 = [f"widgets/{x.name}" for x in files2]
+
+files3 = pathlib.Path("console").glob("*.py")
+files3 = [f"console/{x.name}" for x in files3]
+
+all_files = []
+all_files.extend(files1)
+all_files.extend(files2)
+all_files.extend(files3)
+
+run(f"pyside6-lupdate {' '.join(all_files)} -ts translation_zh_CN.ts")
 run("pyside6-linguist translation_zh_CN.ts")
 run("pyside6-lrelease translation_zh_CN.ts")
