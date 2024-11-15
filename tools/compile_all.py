c97c91dec9f64d9f77f2e14e8b3207b55b978fb3
wjxa20152015 <wjxa20152015@gmail.com>
2024-11-15 09:50:13 +0000

    minor fix

--- tools/compile_all.py
+++ tools/compile_all.py
@@ -127,7 +127,7 @@ def build_help():
     files = ["help.html", "help_en.html"]
 
     for file in files:
-        r = ROOT.joinpath(file).read_text(encoding="utf8")
+        r = Path(__file__).parent.joinpath(file).read_text(encoding="utf8")
         dest_file = PROJ.joinpath(file.replace(".", "_") + ".py")
         r = 'help = r"""\n' + r + 'r"""'
         dest_file.write_text(r, encoding="utf8")
